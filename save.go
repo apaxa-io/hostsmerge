2e6b0cba462140a15fcf60fb58da685d7ee39d21
apaxa-io <bav@wwww4.ru>
2016-11-04 13:59:09 +0300

    Fix permission problem with tmp file (switch to TempFile()). Add config.json.

--- save.go
+++ save.go
@@ -1,31 +1,32 @@
 package main
 
 import (
+	"io/ioutil"
 	"log"
 	"os"
 )
 
-const newSuffix = ".new"
-
 func save(responseChan <-chan string, conf config) {
 	var f *os.File
 	var err error
-	if f, err = os.Create(conf.TargetFile + newSuffix); err != nil {
+	if f, err = ioutil.TempFile("", "hostsmerge.hosts."); err != nil {
 		log.Panic(err)
 		return
 	}
-	defer f.Close()
 
 	fqdnsCount := 0
 	for response := range responseChan {
 		if _, err = f.WriteString(conf.IP + " " + response + "\n"); err != nil {
+			f.Close()
 			log.Panic(err)
 			return
 		}
 		fqdnsCount++
 	}
+	fn := f.Name()
+	f.Close()
 
-	if err = os.Rename(conf.TargetFile+newSuffix, conf.TargetFile); err != nil {
+	if err = os.Rename(fn, conf.TargetFile); err != nil {
 		log.Printf("Unable to rename tmp file to target file: %v", err)
 	}
 
